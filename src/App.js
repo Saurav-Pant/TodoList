39e488dc5dbe26a54c840b887d21c12009ce970e
Saurav Pant <pantsaurav005@gmail.com>
2023-01-16 13:56:50 +0530

    Edit Functionality added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ function App() {
   const [input, setInput] = useState("");
   const [todo, setTodo] = useState([]);
   const [id, setId] = useState(uuidv4());
+  const [edit, setEdit] = useState(null);
+  const [Toggle, setToggle] = useState(true);
+  const [isChecked, setIsChecked] = useState(false);
+  // const [complete, setComplete] = useState(false);
+
+  const [alertStyle, setAlertStyle] = useState({
+    backgroundColor: "#5dadf8",
+  });
+
+  function handleCheckboxChange(event) {
+    setIsChecked(event.target.checked);
+    setAlertStyle(
+      isChecked
+        ? { backgroundColor: "#5dadf8" }
+        : { backgroundColor: "#03C988" }
+    );
+  }
 
   const storingInput = (e) => {
     setInput(e.target.value);
@@ -15,7 +32,19 @@ function App() {
 
   const handleReloading = (e) => {
     e.preventDefault();
-    if (input !== "") {
+    if (!input) {
+      alert("De de");
+    } else if (input && !Toggle) {
+      setTodo(
+        todo.map((e) => {
+          if (e.id === edit) return { ...e, input: input };
+          return e;
+        })
+      );
+      setToggle(true);
+      setInput("");
+      setEdit(null);
+    } else {
       setTodo([{ id: id, input }, ...todo]);
       setId(uuidv4());
       setInput("");
@@ -24,10 +53,17 @@ function App() {
 
   const handleDelete = (id) => {
     const delTodo = todo.filter((d) => d.id !== id);
+    console.log(delTodo);
     setTodo([...delTodo]);
   };
-  
- 
+
+  const handleEdit = (id) => {
+    const editTodo = todo.find((e) => e.id === id);
+    console.log(editTodo.input);
+    setInput(editTodo.input);
+    setToggle(false);
+    setEdit(id);
+  };
 
   return (
     <div className="whole_todo">
@@ -36,13 +72,19 @@ function App() {
         input={input}
         storingInput={storingInput}
         handleReloading={handleReloading}
+        Toggle={Toggle}
+        setToggle={setToggle}
       />
       <TodoList />
       <TodoShow
         todo={todo}
         handleDelete={handleDelete}
         input={input}
-        // handleEdit={handleEdit}
+        handleEdit={handleEdit}
+        isChecked={isChecked}
+        handleCheckboxChange={handleCheckboxChange}
+        alertStyle={alertStyle}
+        // complete={complete}
       />
     </div>
   );
--- src/Components/TodoShow.js
+++ src/Components/TodoShow.js
@@ -2,17 +2,32 @@ import React from "react";
 import "../Styles/TodoShow.scss";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 
-function TodoShow({ todo, handleDelete }) {
+function TodoShow({
+  todo,
+  handleDelete,
+  handleEdit,
+  isChecked,
+  handleCheckboxChange,
+  alertStyle,
+  complete,
+}) {
   return (
     <div className="TodoShow">
       <ul className="all">
         {todo.map((t) => (
-          <li className="wholeTodo" key={t.id}>
+          <li className="wholeTodo" key={t.id} style={alertStyle}>
             <span className="SingleTodo">{t.input}</span>
             <label className="done">
-              <input type="checkbox" id="checkbox" className="input1" />
+              <input
+                type="checkbox"
+                id="checkbox"
+                className="input1"
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+                // complete={complete}
+              />
             </label>
-            <button className="edit">
+            <button className="edit" onClick={() => handleEdit(t.id)}>
               <AiFillEdit style={{ color: "green", height: "10px" }} />
             </button>
             <button className="delete" onClick={() => handleDelete(t.id)}>
